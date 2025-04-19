8d2351a7e658bd37e5803ba43d938c205dd03bf6
Kate <kit-ty-kate@outlook.com>
2025-04-19 20:56:45 +0100

    improve

--- src/lib.ml
+++ src/lib.ml
@@ -1,39 +1,40 @@
 module String = struct
-  let is_prefix ~prefix str =
-    let prefix_len = String.length prefix in
-    let str_len = String.length str in
-    let rec aux i prefix_len str_len prefix str =
+  let is_prefix =
+    let rec aux i ~prefix_len ~prefix ~str =
       if i < (prefix_len : int) then
         if String.unsafe_get str i =
            (String.unsafe_get prefix i : char) then
-          aux (i + 1) prefix_len str_len prefix str
+          aux (i + 1) ~prefix_len ~prefix ~str
         else
           false
       else
         true
     in
-    if prefix_len <= (str_len : int) then
-      aux 0 prefix_len str_len prefix str
-    else
-      false
+    fun ~prefix str ->
+      let prefix_len = String.length prefix in
+      if prefix_len <= (String.length str : int) then
+        aux 0 ~prefix_len ~prefix ~str
+      else
+        false
 
-  let is_suffix ~suffix str =
-    let suffix_len = String.length suffix in
-    let str_len = String.length str in
-    let rec aux suffix_i str_i suffix_len str_len suffix str =
+  let is_suffix =
+    let rec aux suffix_i str_i ~suffix_len ~suffix ~str =
       if suffix_i < (suffix_len : int) then
         if String.unsafe_get str str_i =
            (String.unsafe_get suffix suffix_i : char) then
-          aux (suffix_i + 1) (str_i + 1) suffix_len str_len suffix str
+          aux (suffix_i + 1) (str_i + 1) ~suffix_len ~suffix ~str
         else
           false
       else
         true
     in
-    if suffix_len <= (str_len : int) then
-      aux 0 (str_len - suffix_len) suffix_len str_len suffix str
-    else
-      false
+    fun ~suffix str ->
+      let suffix_len = String.length suffix in
+      let str_len = String.length str in
+      if suffix_len <= (str_len : int) then
+        aux 0 (str_len - suffix_len) ~suffix_len ~suffix ~str
+      else
+        false
 
   let cut sep str =
     match String.index_opt str sep with
